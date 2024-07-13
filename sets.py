db930571a7189adda8289c697f2149f92f35c448
nidak106 <144581842+nidak106@users.noreply.github.com>
2024-07-13 20:23:52 +0500

    py

---
+++ sets.py
@@ -0,0 +1,54 @@
+# my_string="hello"
+# if 'e' in my_string:
+#   print("yes")
+# else:
+#     print("no")  
+
+name="nida khan"
+#name=name.split()#removes spaces in the string 
+
+#print(name.upper())
+#print(name.lower())
+#print(name.startswith('n'))
+#print(name.endswith("a"))
+#print(name.find('d'))
+#print(name.count('a'))
+#print(name.replace('khan','affan'))
+
+
+# from timeit import default_timer as timer
+# my_list=['a']*4
+# my_string=""
+
+# #bad python code
+# start=timer()
+# for i in my_list:
+#     my_string+=i
+# print(my_string)  
+# stop=timer()
+# print("time:",stop-start)
+
+# #good python code
+# start=timer()
+# my_string2=''.join(my_list)
+# print(my_string2)
+# stop=timer()
+# print("time:" ,stop-start)
+
+var="tom"
+# my_string="variable is %s" %var
+# print(my_string)
+#2nd
+var2=3
+# my_string="variable is a %d" %var2
+# print(my_string)
+
+#3rd
+var3=3.4566
+my_string="variable is %f" %var3
+str="variable is %.2f" %var3
+print(my_string)
+print(str)
+
+
+
